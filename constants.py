56417add5d9c2f57ef4c494aec33a513398b9d8f
Josh Mills <jmills@paperlessolutions.net>
2017-11-09 08:33:45 -0800

    Added teams to the constants file and updated user prompting for main page. Now, the user can provide the league and team name in order to request data for the specific team.

--- constants.py
+++ constants.py
@@ -1,1 +1,14 @@
 # This class will house the mappings and constants for names and other soccer specific data.
+    
+leagues = ["bundesliga_teams", "premier_league_teams"];
+
+bundesliga_teams = {"FC Koln" : "1", "TSG 1899 Hoffenheim" : "2", "Bayer Leverkusen" : "3", "Borussia Dortmund" : "4", 
+    "FC Bayern Munchen" : "5", "FC Schalke 04" : "6", "Hamburger SV" : "7", "Hannover 96" : "8", 
+    "Hertha BSC" : "9", "VfB Stuttgart" : "10", "VfL Stuttgart" : "11", "Werder Bremen" : "12", 
+    "FC Kaiserslautern" : " 13", "FC Nurnberg" : "14", "FSV Mainz" : "15", "FC Augsburg" : "16",
+    "SC Freiburg" : "17", "Borussia Monchengladbach" : "18", "Eintracht Frankfurt" : "19"}
+
+premier_league_teams = {"Arsenal FC" : "57", "Aston Villa FC" : "58", "Blackburn Rovers" : "59", 
+    "Bolton Wanderers FC" : "60", "Chelsea FC" : "61", "Everton FC" : "62",
+    "Fulham FC" : "63", "Liverpool FC" : "64", "Machester City FC" : "65",
+    "Manchester United FC" : "66"}
--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 # This class will house the basic underlying logic for the application.
 
-print ("Hello and welcome to millsjb Soccer Application");
+import constants
 
-team = input("What team would you like to see the most recent info for?");
+print("Hello and welcome to millsjb Soccer Application")
+print("")
 
-if (team != null and team.isEmpty() == false):
-    print("Thanks, please wait for " + team + "'s info.");
+league = raw_input("What league would you like to select?  ")
 
+team = raw_input("What team would you like to see the most recent info for?  ")
+
+if (team is None and team != ""):
+    print("Thanks, please wait for " + team + "'s info.")
+    
+# selected_league = null #used for constants file TEMP
+# 
+# for l in constants.leagues:
+#     if(l.contains(league)):
+#         selected_league = l
+#         
+# print(selected_league)
+
+
+
+if("bundesliga" in league):
+    team = constants.bundesliga_teams[team]
+if("premier" in league):
+    team = constants.premier_league_teams[team]
+    
+    
 # now call the function to build the url and make the appropriate api call
-response = getTeamInfo(team);
+response = getTeamInfo(team)
